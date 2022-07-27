967429dba9aeb021187907db8dee1c687e8d9385
Spirlhark <Spirlhark@gmail.com>
2022-07-27 13:10:19 +0300

    commit 10

--- pages/PeopleSharingPage.py
+++ pages/PeopleSharingPage.py
@@ -6,7 +6,8 @@ from selenium.webdriver.common.by import By
 class PeopleSharingPage(BasePage):
     locator_dictionary = {
         "tabPeopleSharingPage": (By.CSS_SELECTOR, ".HJOYVi12"),
-        "openContactsPage": (By.PARTIAL_LINK_TEXT, 'Начать')
+        "openContactsPage": (By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/c-wiz/c-wiz/div/div[3]/div/div/c-wiz/section/div[1]/div/div/div[2]/div[2]/a/div')
+        # "openContactsPage": (By.PARTIAL_LINK_TEXT, 'Начать')
         }
 
     @allure.step("Tab People Sharing Page")
